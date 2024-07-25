d71379be9347dd6882243e47e2376e89324dc855
IOENKIS <90310655+IOENKIS@users.noreply.github.com>
2024-07-25 11:05:25 +0300

    Initial Commit

---
+++ Bass Booster Volume Amplifire/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Bass Booster Volume Amplifire
+//
+//  Created by IVANKIS on 25.07.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
